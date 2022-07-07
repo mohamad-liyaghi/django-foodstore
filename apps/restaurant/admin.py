2973a98b5d7053b951da5ed17b2d4f389711e573
Ml06py <liaghimohamad69@gmail.com>
2022-07-07 13:31:35 +0430

    Food model added

--- apps/restaurant/admin.py
+++ apps/restaurant/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Restaurant
+from .models import Restaurant, Food, Category
 # Register your models here.
 
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ("name", "owner", "email")
     prepopulated_fields = {'slug':('name',)}
 
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ("name" ,"price", "is_available")
+    prepopulated_fields = {"slug" : ("name", "provider")}
+
 admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Category)
--- apps/restaurant/models.py
+++ apps/restaurant/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
 from star_ratings.models import Rating
+from django.urls import reverse
 from accounts.models import User
 # Create your models here.
 
@@ -20,4 +21,37 @@ class Restaurant(models.Model):
 
     ratings = GenericRelation(Rating)
 
+    def get_absolute_url(self):
+        return reverse("restaurant:profile-restaurant", kwargs={"pk" : self.pk, "slug" : self.slug})
 
+    def __str__(self):
+        return  self.name
+
+
+
+class Food(models.Model):
+    name = models.CharField(max_length= 10)
+    slug = models.SlugField(unique=True, blank=True)
+
+    picture = models.ImageField(upload_to="foods/")
+
+    description = models.TextField(max_length=30)
+    category = models.ManyToManyField("Category", related_name="food_category")
+    provider = models.ForeignKey(Restaurant, on_delete=models.CASCADE, blank=True, related_name="foods")
+
+    inventory = models.PositiveIntegerField(default=0)
+
+    price = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+
+    ratings = GenericRelation(Rating)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
