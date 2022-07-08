de77196ee06c2d2950cb91a1fadc18e5071a4c90
Ml06py <liaghimohamad69@gmail.com>
2022-07-08 19:44:58 +0430

    Home page and search page added

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import HomePageView, FoodSearchView
+
+app_name = "customer"
 
 urlpatterns =[
-
+    path("", HomePageView.as_view(), name= "home"),
+    path("foods/", FoodSearchView.as_view(), name="food-result"),
 ]
--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
 
-# Create your views here.
+from restaurant.models import Food, Category
+
+
+class HomePageView(TemplateView):
+    '''
+        Home page
+    '''
+    template_name = "customer/home.html"
+
+class FoodSearchView(ListView):
+    '''
+        Show result of searched data
+    '''
+    template_name = "customer/food-list.html"
+    context_object_name = "foods"
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', None)
+        return Food.objects.filter(
+            Q(name__icontains=q) | Q(category__title= q)
+        ).order_by("-is_available")
