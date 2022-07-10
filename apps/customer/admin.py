65238b33b489ff3ec72cc330339ac9d35f1a8446
Ml06py <liaghimohamad69@gmail.com>
2022-07-10 18:48:41 +0430

    Order model added

--- apps/customer/admin.py
+++ apps/customer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("orderid", "user", "is_paid", "status")
+
+
+admin.site.register(Order, OrderAdmin)
--- apps/customer/models.py
+++ apps/customer/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from restaurant.models import  Food
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Order(models.Model):
+    class status(models.TextChoices):
+        ordered = "ordered"
+        preparing = "preparing"
+        sending = "sending"
+        arrived = "Arrived"
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
+    items = models.ManyToManyField(Food, blank=True)
+    orderid = models.CharField(max_length=15, blank=True)
+    total_price = models.IntegerField(blank=True)
+
+    is_paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(choices=status.choices, default=status.ordered, max_length= 10)
+
+    def __str__(self):
+        return self.orderid
+
