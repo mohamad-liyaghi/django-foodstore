1ebe71833a0296b72ab2f5003328df9cbc7f0289
Ml06py <liaghimohamad69@gmail.com>
2022-12-12 11:21:32 +0330

    OrderItem model added

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -13,16 +13,33 @@ class Order(models.Model):
         arrived = "Arrived"
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
-    items = models.ManyToManyField(Food, blank=True)
-    prepared_items = models.ManyToManyField(Food, blank=True, related_name="prepared_items")
-    orderid = models.CharField(max_length=15, default=random_number)
-    total_price = models.IntegerField(blank=True)
+    order_id = models.CharField(max_length=15, default=random_number)
 
     is_paid = models.BooleanField(default=False)
 
     date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(choices=status.choices, default=status.ordered, max_length= 10)
+    status = models.CharField(choices=status.choices, default=status.ordered, max_length=10)
+
+    @property
+    def total_price(self):
+        return sum(item.price() for item in self.items.all())
 
     def __str__(self):
-        return str(self.orderid)
+        return str(self.order_id)
 
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    item = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField(default=1)
+    
+    prepared = models.BooleanField(default=False)
+
+    @property
+    def price(self):
+        return self.item.price * self.quantity
+
+    def __str__(self):
+        return self.order.order_id
